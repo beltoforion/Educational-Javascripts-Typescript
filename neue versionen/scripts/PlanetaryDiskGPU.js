48328378731ccddc547497af08b83d78fc79881c
Ingo Berg <github@beltoforion.de>
2026-08-25 18:16:55 +0200

    lizenze updated; unused files removed

--- neue versionen/scripts/PlanetaryDiskGPU.js
+++ neue versionen/scripts/PlanetaryDiskGPU.js
@@ -1,3 +1,27 @@
+/*
+ * Educational-Javascript-Typescript
+ * https://github.com/beltoforion/Educational-Javascripts-Typescript
+ *
+ * Copyright (c) 2026, Ingo Berg
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the “Software”), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 // Planetary disk simulation (GPU). 2D leapfrog integrator for ~10^5
 // massless tracers perturbed by a handful of CPU-stepped planets. Tracer
 // integration runs on the GPU via WebGL2 transform feedback; the CPU only
